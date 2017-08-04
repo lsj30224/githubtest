412675c9f3f402565b01cf1af1848cc4ae247399
lsj30224 <30730056+lsj30224@users.noreply.github.com>
2017-08-05 02:31:44 +0900

    add scanf

--- githubtest/main.c
+++ githubtest/main.c
@@ -14,8 +14,8 @@ int main(void)
 
     printf("github test1\n");  //이제 있음
     printf("input >>");
-    //scanf("%s", str);
-    //printf("output : %s\n", str);
+    scanf("%s", str);
+    printf("output : %s\n", str);
     //printf("branch 전환\n");
     //printf("1\n");
     return 0;
