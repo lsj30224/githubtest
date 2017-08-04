aaabe300a59576db494a901c0d154091e93cd412
Seungjun <seungjun@Seungjunui-MacBook-Pro.local>
2017-08-05 01:35:53 +0900

    Initial Commit

---
+++ githubtest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  githubtest
+//
+//  Created by Seungjun on 2017. 8. 5..
+//  Copyright © 2017년 Seungjun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
