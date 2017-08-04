11b7194e14b683a9836d66070d8c986da470fcb7
lsj30224 <30730056+lsj30224@users.noreply.github.com>
2017-08-05 02:40:27 +0900

    ㅁㄴㅇ

--- githubtest/main.c
+++ githubtest/main.c
@@ -18,5 +18,9 @@ int main(void)
     printf("output : %s\n", str);
     //printf("branch 전환\n");
     //printf("1\n");
+    for(int i = 0; i < 10; i++)
+        printf("%d", i);
+    printf("\n");
     return 0;
 }
+//commit을 하면 수정사항이 로컬로 들어가고 push를 하면 commit한게 원격저장소로 올라가는듯
